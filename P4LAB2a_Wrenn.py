1c0830ec7f8dc9a743a9b617499dcb799df246a6
BrieWrenn <99675276+BrieWrenn@users.noreply.github.com>
2022-04-04 22:53:05 -0400

    Add files via upload

---
+++ P4LAB2a_Wrenn.py
@@ -0,0 +1,13 @@
+
+import turtle
+for x in range (4):
+    turtle.forward (50)
+    turtle.left (90)
+    turtle.forward (50)
+    turtle.left (90)
+
+for x in range (3):
+    turtle.forward (50)
+    turtle.left (120)
+    turtle.forward (50)
+    
